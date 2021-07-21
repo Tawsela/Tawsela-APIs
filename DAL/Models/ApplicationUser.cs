c4407a6c9fdb6794ddbbf69606fcdc3ac9248f25
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-07-21 16:42:19 +0200

    Add files via upload

---
+++ DAL/Models/ApplicationUser.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+      public  class ApplicationUser:IdentityUser
+    {
+       
+       
+
+        [Required]
+        public int SSN { get; set; }
+        [Required]
+        public string FullName { get; set; }
+       
+        public string ImagePath { get; set; }
+        public string TripId { get; set; }
+       
+        public int CarId { get; set; }
+
+
+        [ForeignKey("CarId")]
+        public virtual Car Car { get; set; }
+
+
+        [InverseProperty("Clients")]
+        public virtual Trip User { get; set; }
+        [InverseProperty("Captain")]
+        public virtual Trip Driver { get; set; }
+
+
+
+    }
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext():base()
+        {
+
+        }
+        public ApplicationDBContext (DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-7U1FJEF\SQLSERVERMARK;Initial Catalog='Wasalny';Integrated Security=True");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+
+    }
+   
+   
+}
---
+++ DAL/Models/Trip.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+   public class Trip
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CaptainId { get; set; }
+        [Required]
+        
+        public int ClientNumber { get; set; }
+
+        [Required]
+        public string LocationFrom { get; set; }
+
+        [Required]
+        public string LocationTo { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DepartialTime { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime ArrivalTime { get; set; }
+
+        [InverseProperty("Driver")]
+        [ForeignKey("CaptainId")]
+        public virtual ApplicationUser Captain { get; set; }
+        [InverseProperty("User")]
+        [ForeignKey("ClientNumber")]
+        public virtual ICollection<ApplicationUser> Clients { get; set; }
+
+    }
+}
